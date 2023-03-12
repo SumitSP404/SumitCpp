6eb92e7f50085c8aff33029257edfb935c9dedfa
Sumit S Purandare <122196568+SumitSP404@users.noreply.github.com>
2023-03-12 14:47:13 +0530

    Add files via upload

---
+++ Pattern17.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int row = 1;
+    while(row <= n){
+        //printing the space
+        int space = n - row;
+        while(space){
+            cout<<" ";
+            space = space - 1;
+        }
+        int col = 1;
+        while(col<=row){
+            //printing the star
+            cout<<"*";
+            col = col + 1;
+        }
+        cout<<endl;
+        row = row + 1;
+    }
+}
